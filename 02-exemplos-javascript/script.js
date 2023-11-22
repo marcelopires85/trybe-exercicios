d50ec2d4f606cf7170c12b60a67baf377415395c
Marcelo Vidal <marcelovidal85@gmail.com>
2023-11-22 16:22:25 -0300

    Exercício Javascript 1

--- 02-exemplos-javascript/script.js
+++ 02-exemplos-javascript/script.js
@@ -1,1 +1,45 @@
-console.log("Olá mundo!");
+//Exercício [1]
+
+//Adição
+
+const num1 = 10;
+const num2 = 5;
+
+const adicao = num1 + num2;
+
+console.log(adicao);
+
+//Subtração
+
+const subtracao = num1 - num2;
+
+console.log(subtracao);
+
+//Multiplicação
+
+const multiplicacao = num1 * num2;
+
+console.log(multiplicacao);
+
+//Divisão
+
+const divisao = num1 / num2;
+
+console.log(divisao);
+
+//Módulo
+
+const modulo = num1 % num2;
+
+console.log(modulo);
+
+//Forma resumida de resolver o mesmo problema
+
+let num01 = 10;
+let num02 = 5;
+
+console.log("Soma: " + (num01 + num02));
+console.log("Subtração: " + (num01 - num02));
+console.log("Multiplicação: " + num01 * num02);
+console.log("Divisão: " + num01 / num02);
+console.log("Módulo: " + (num01 % num02));
