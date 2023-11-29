6c907de1c97f8850737f8db2e52999c4739e10d8
Marcelo Vidal <marcelovidal85@gmail.com>
2023-11-29 14:55:26 -0300

    Foi adicionado arquivo de exercícios loop for

---
+++ 03-arrays-e-loop-for/exercicios.js
@@ -0,0 +1,23 @@
+//Exercícios 1
+
+const amazonList = [
+  "Escova",
+  "Pasta de Dente",
+  "Detergente",
+  "Sabonete Phebo",
+  "Molho de Tomate",
+];
+
+for (let index = 0; index < amazonList.length; index += 1) {
+  const itemCorrente = amazonList[index];
+  console.log(itemCorrente);
+}
+
+//Exercícios 2
+
+const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 0; index < numbers.length; index += 1) {
+  const numbersImprime = numbers[index];
+  console.log(numbersImprime);
+}
