e1a07bad2e5270e9f71722d9943c8b7c80f17382
Marcelo Vidal <marcelovidal85@gmail.com>
2023-12-13 21:07:20 -0300

    Adicionado exercício de refatoramento da Mega-Sena com Função

--- 04-funcoes/funcoes.js
+++ 04-funcoes/funcoes.js
@@ -48,3 +48,44 @@ Maior: ${maiorNumero}`;
 
 console.log(maiorMenorNumero([1, 2, 3, 8, 15, -5, 0, 21]));
 console.log(maiorMenorNumero([1, 18, 32, 85, 151, -57, 0, 213, 25, 35]));
+
+//Refatoramento do código da Mega-Sena utilizando Função
+
+const lotteryNumbers = [];
+const firstGame = [4, 8, 15, 16, 23, 42];
+
+// Sorteio dos números
+const gerarNumerosLoteria = (quantidade, numeroMaximo, array) => {
+  for (let index = 0; index < quantidade; index += 1) {
+    let randomNumber = Math.ceil(Math.random() * numeroMaximo);
+    if (array.includes(randomNumber)) {
+      index -= 1;
+    } else {
+      array.push(randomNumber);
+    }
+  }
+};
+gerarNumerosLoteria(18, 99, lotteryNumbers);
+
+// Comparação do sorteio com o meu jogo
+let numberOfHits = 0;
+let hits = [];
+
+for (
+  let indexLottery = 0;
+  indexLottery < lotteryNumbers.length;
+  indexLottery += 1
+) {
+  for (let indexGame = 0; indexGame < firstGame.length; indexGame += 1) {
+    if (lotteryNumbers[indexLottery] === firstGame[indexGame]) {
+      numberOfHits += 1;
+      hits.push(lotteryNumbers[indexLottery]);
+    }
+  }
+}
+
+// Quantidade de acertos
+console.log(`Números sorteados: ${lotteryNumbers}`);
+console.log(`Jogo: ${firstGame}`);
+console.log(`Quantidade de acertos: ${numberOfHits}`);
+console.log(`Números acertados: ${hits}`);
