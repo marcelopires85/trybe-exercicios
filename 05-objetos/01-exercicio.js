285debfdb83f91986b5a27ed770cda4f61a04d90
Marcelo Vidal <marcelovidal@iMac-Pro-de-Marcelo.local>
2024-07-02 17:11:04 -0300

    Foi adicionado arquivo sobre Objetos e primeiro exercício.

---
+++ 05-objetos/01-exercicio.js
@@ -0,0 +1,56 @@
+//Objetos
+
+//Informações Macdonalds - Descrição com várias constantes
+
+// const bigMac = 26.90;
+// const bigTasty = 39.90;
+// const mcFritas = 11.90;
+// const topSunday = 13.90;
+// const isDelicious = true;
+// const city = 'São Paulo';
+// const state = 'SP';
+// const unitName = 'Shopping Recife';
+
+//Criação com objetos do exemplo acima
+//Para navegar com os objetos no console via propriedades do objeto, usar os pontos ou então [' ']
+//Além das propriedades existe as chaves (Object.keys) e os valores (Object.values). Retornam como um array com as informações
+//A também outra propriedade chamada entradas (Object.entries), ele retorna as chaves e os valores como forma de array (um array com arrays contendo as infomações)
+
+const Macdonalds = {
+  bigMac: 26.9,
+  BigTasty: 39.9,
+  mcFritas: 11.9,
+  topSunday: 13.9,
+  isDelicious: true,
+  adress: {
+    city: "São Paulo",
+    state: "SP",
+    unitName: "Shopping Recife",
+  },
+};
+
+// console.log(Macdonalds.mcFritas);
+// console.log(Object.keys(Macdonalds));
+// console.log(Object.values(Macdonalds));
+// console.log(Object.entries(Macdonalds));
+
+//Exercício 01
+
+const cardapio = require("./data");
+
+const getMacInfo = () => {
+  const report = {
+    sanduiches: 0,
+    acompanhamentos: 0,
+    sobremesas: 0,
+    bebidas: 0,
+  };
+  const keys = Object.keys(cardapio);
+
+  for (let index = 0; index < keys.length; index += 1) {
+    report[keys[index]] = cardapio[keys[index]].length;
+  }
+  return report;
+};
+
+console.log(getMacInfo());
---
+++ 05-objetos/data.js
@@ -0,0 +1,118 @@
+module.exports = {
+  sanduiches: [
+    {
+      name: "Big Mac",
+      price: 26.9,
+      ingredients: [
+        "2 hambúrgueres",
+        "alface americana",
+        "queijo cheddar",
+        "molho especial",
+        "cebola",
+        "picles",
+        "pão com gergelim",
+      ],
+      calories: 503,
+    },
+    {
+      name: "Duplo Quarterão",
+      price: 39.9,
+      ingredients: [
+        "2 hambúrgueres",
+        "mostarda",
+        "ketchup",
+        "cebola",
+        "queijo cheddar",
+        "pão com gergelim",
+      ],
+      calories: 762,
+    },
+    {
+      name: "Big Tasty",
+      price: 39.9,
+      ingredients: [
+        "1 hambúrguer",
+        "queijo emental",
+        "tomate",
+        "alface americana",
+        "cebola",
+        "molho tasty",
+        "pão com gergelim",
+      ],
+      calories: 944,
+    },
+  ],
+  acompanhamentos: [
+    {
+      name: "Chicken McNuggets - 10 unidades",
+      price: 16.9,
+      ingredients: ["frango empanado"],
+      calories: 414,
+    },
+    {
+      name: "McFritas Kids",
+      price: 8.9,
+      ingredients: ["batata", "sal"],
+      calories: 90,
+    },
+    {
+      name: "McFritas Pequena",
+      price: 11.9,
+      ingredients: ["batata", "sal"],
+      calories: 211,
+    },
+    {
+      name: "McFritas Média",
+      price: 13.9,
+      ingredients: ["batata", "sal"],
+      calories: 295,
+    },
+    {
+      name: "McFritas Grande",
+      price: 15.9,
+      ingredients: ["batata", "sal"],
+      calories: 422,
+    },
+  ],
+  sobremesas: [
+    {
+      name: "McFlurry Ovomaltine Caramelo",
+      price: 13.9,
+      ingredients: [
+        "bebida láctea sabor baunilha",
+        "flocos de ovomaltine",
+        "cobertura de caramelo",
+      ],
+      calories: 462,
+    },
+    {
+      name: "Top Sundae Chocolate",
+      price: 11.9,
+      ingredients: [
+        "bebida láctea sabor baunilha",
+        "cobertura de chocolate",
+        "farofa de paçoca",
+        "canudo",
+      ],
+      calories: 377,
+    },
+    {
+      name: "Torta de Banana",
+      price: 6.9,
+      ingredients: ["banana", "massa crocante"],
+      calories: 222,
+    },
+  ],
+  bebidas: [
+    {
+      name: "Coca-Cola 300ml",
+      price: 13.9,
+      calories: 129,
+    },
+    {
+      name: "Del Valle Uva 500ml",
+      price: 11.9,
+      calories: 200,
+    },
+  ],
+};
