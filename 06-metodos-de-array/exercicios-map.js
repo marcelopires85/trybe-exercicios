c96d714b1af1c912506128f2589fd5a38b1e8f33
Marcelo Vidal <marcelovidal@iMac-Pro-de-Marcelo.local>
2024-07-25 16:34:11 -0300

    Adicionados todos exercícios utilizando map em objetos.

---
+++ 06-metodos-de-array/exercicios-map.js
@@ -0,0 +1,34 @@
+//Exercício Conversão de Temperatura
+
+// Temperaturas em graus Celsius
+const temperaturesCelsius = [23, 10, 32, 21, 47];
+
+const convertToFahrenheit = (temperaturesCelsius) => {
+  let temperaturesFahrenheit = temperaturesCelsius.map((temperature) => {
+    return (temperature * 9) / 5 + 32;
+  });
+
+  return temperaturesFahrenheit;
+};
+
+console.log(convertToFahrenheit(temperaturesCelsius));
+
+//Exercício Raíz Quadrada
+
+const numbers = [4, 9, -1, 16, -5, 25];
+
+const calculateSquareRoots = (numbers) => {
+  let squareRoots = numbers.map((number) => {
+    if (number >= 0) {
+      return Math.sqrt(number);
+    } else {
+      return NaN;
+    }
+  });
+
+  return squareRoots;
+};
+
+console.log(calculateSquareRoots(numbers));
+
+//Exercício de Categorização de Livros
